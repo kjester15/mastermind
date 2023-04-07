9a6cbc9a7ad11d3192c6491c71902c26b6b071c6
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2023-04-07 14:02:03 -0600

    Create check_answer method which returns a clue based on accuracy of user guess

--- mastermind.rb
+++ mastermind.rb
@@ -1,10 +1,13 @@
 class Board
-    attr_accessor :answer, :guess
+    attr_accessor 
+    attr_reader :round, :guess, :answer, :clue
 
     def initialize
         @symbols = ["1", "2", "3", "4", "5", "6"]
         @answer = [@symbols.sample, @symbols.sample, @symbols.sample, @symbols.sample]
         @guess = []
+        @clue = []
+        @round = 1
     end
 
     def get_guess
@@ -16,11 +19,31 @@ class Board
                 @guess[x] = gets.chomp
             end
         end
+        @round += 1
         return @guess
+    end
+
+    def check_answer
+        # x if not in right spot or in answer at all
+        # o if in answer but not right spot
+        # @ if in answer AND right spot
+        for x in 0..3 do
+            if @guess[x] == @answer[x]
+                @clue[x] = "@"
+            elsif @answer.include?(@guess[x])
+                @clue[x] = "o"
+            else
+                @clue[x] = "x"
+            end
+        end
+        return @clue
     end
 end
 
 current_board = Board.new
 p current_board.answer
 user_guess = current_board.get_guess
-p user_guess
+puts "Your guess is: #{user_guess}"
+clue = current_board.check_answer
+puts "Your clue is: #{clue}"
+
