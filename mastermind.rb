494a99c0f870668fde5d3a2f4937b2ba63006446
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2023-04-07 13:39:13 -0600

    Create get_guess method which gets 4 inputs from user and creates guess array

--- mastermind.rb
+++ mastermind.rb
@@ -1,12 +1,26 @@
 class Board
-    attr_accessor :answer
+    attr_accessor :answer, :guess
 
     def initialize
-        @symbols = ["a", "s", "d", "f", "g", "h"]
+        @symbols = ["1", "2", "3", "4", "5", "6"]
         @answer = [@symbols.sample, @symbols.sample, @symbols.sample, @symbols.sample]
         @guess = []
+    end
+
+    def get_guess
+        for x in 0..3 do
+            puts "x = #{x}"
+            @guess[x] = gets.chomp
+            until @symbols.include?(@guess[x])
+                puts "Please select from the following list of choices: [1, 2, 3, 4, 5, 6]"
+                @guess[x] = gets.chomp
+            end
+        end
+        return @guess
     end
 end
 
 current_board = Board.new
 p current_board.answer
+user_guess = current_board.get_guess
+p user_guess
