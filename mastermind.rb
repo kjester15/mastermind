b53b9c5414f116535103746baf8b80b1adc3bd99
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2023-04-11 15:43:11 -0600

    Create prelim game loop for computer

--- mastermind.rb
+++ mastermind.rb
@@ -1,6 +1,7 @@
 # the Board class represents the game board while the user is in guess mode
 class Board
   attr_reader :round, :guess, :answer, :clue, :win
+  attr_writer :round, :win
 
   def initialize
     @symbols = %w[1 2 3 4 5 6] # use %w for arrays of strings
@@ -83,6 +84,7 @@ class Computer
 
   def initialize
     @solutions = []
+    @guess = ''
   end
 
   def populate_solutions
@@ -101,58 +103,72 @@ class Computer
       end
     end
   end
+
+  def guess_code
+    @guess = ''
+    4.times do
+      number = rand(1..6).to_s
+      @guess += number
+    end
+    puts "Computer's guess is #{@guess}."
+  end
 end
 
 while
-    game_mode = ''
-    puts "Welcome to 'Mastermind'! Would you like to create a secret code for the computer to guess, \
+  game_mode = ''
+  puts "Welcome to 'Mastermind'! Would you like to create a secret code for the computer to guess, \
 or guess the computer's secret code? Type 'A' for create or 'B' for guess"
-    # ask user if they want to create secret code or guess
-    until game_mode == "A" || game_mode == "B"
-      game_mode = gets.chomp.upcase
-      break if game_mode == "A" || game_mode == "B"
+  # ask user if they want to create secret code or guess
+  until game_mode == "A" || game_mode == "B"
+    game_mode = gets.chomp.upcase
+    break if game_mode == "A" || game_mode == "B"
 
-      puts "Please enter a valid option: 'A' to create a secret code, or 'B' \
+    puts "Please enter a valid option: 'A' to create a secret code, or 'B' \
 to guess the secret code"
-    end
+  end
 
-    if game_mode == 'A' # create code mode
-      current_computer = Computer.new
-      current_player = Player.new
-      current_computer.populate_solutions
-      current_player.create_code
-    elsif game_mode == 'B' # guess code mode
-      # create new board
-      current_board = Board.new
-      # explain rules
-      puts "You have 12 rounds to figure out what the 4 symbol code is. An 'x' indicates your guess \
+  if game_mode == 'A' # create code mode
+    current_board = Board.new
+    current_computer = Computer.new
+    current_player = Player.new
+    current_computer.populate_solutions
+    current_player.create_code
+    while current_board.round < 13 && !current_board.win
+      current_computer.guess_code
+      current_board.round += 1
+    end
+  elsif game_mode == 'B' # guess code mode
+    # create new board
+    current_board = Board.new
+    # explain rules
+    puts "You have 12 rounds to figure out what the 4 symbol code is. An 'x' indicates your guess \
 is not in the answer at all, an 'o' indicates your guess is in the answer but wasn't in the correct spot, \
 and an '@' indicates you've guessed the correct symbol in the correct spot. Your available options are: 1 2 3 4 5 & 6. \
 Good luck!"
-      # error check - print answer (DELETE)
-      p current_board.answer
+    # error check - print answer (DELETE)
+    p current_board.answer
 
-      # loop until user runs out of guesses or user guesses the correct answer
-      while current_board.round < 13 && !current_board.win
-        puts "Round: #{current_board.round}"
-        user_guess = current_board.guess_symbol
-        puts "Your guess is: #{user_guess}"
-        clue = current_board.create_clue
-        puts "Your clue is: #{clue}"
-        current_board.check_for_win
-      end
-      # display lose text if user didn't win
-      if current_board.win == false then puts "You lost! The correct answer was: #{current_board.answer}" end
+    # loop until user runs out of guesses or user guesses the correct answer
+    while current_board.round < 13 && !current_board.win
+      puts "Round: #{current_board.round}"
+      user_guess = current_board.guess_symbol
+      puts "Your guess is: #{user_guess}"
+      clue = current_board.create_clue
+      puts "Your clue is: #{clue}"
+      current_board.check_for_win
     end
+    # display lose text if user didn't win
+    if current_board.win == false then puts "You lost! The correct answer was: #{current_board.answer}" end
+  end
 
-    # ask to play another game
-    answer = ''
-    until answer == 'yes' || answer == 'no'
-      puts 'Would you like to play again?'
-      answer = gets.chomp.downcase
-      break if answer == 'yes' || answer == 'no'
+  # ask to play another game
+  answer = ''
+  until answer == 'yes' || answer == 'no'
+    puts 'Would you like to play again?'
+    answer = gets.chomp.downcase
+    break if answer == 'yes' || answer == 'no'
 
-      puts "Please answer with 'yes' or 'no'"
-    end
-    break if answer == 'no'
+    puts "Please answer with 'yes' or 'no'"
+  end
+  break if answer == 'no'
 end
